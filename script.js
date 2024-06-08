8f565dca3c0e5a93ace423ce9c4c0de72336934e
isaacvadij <isaacolando8@gmail.com>
2024-06-08 23:51:03 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // JavaScript for smooth scrolling
+    document.querySelectorAll('nav ul li a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
